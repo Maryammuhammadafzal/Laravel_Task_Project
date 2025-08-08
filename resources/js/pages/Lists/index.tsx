2ad943e7eff03c88116ef73f5ecb3b17429a2d16
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-08-08 18:55:45 +0500

    designing a lists page

--- resources/js/pages/Lists/index.tsx
+++ resources/js/pages/Lists/index.tsx
@@ -95,7 +95,13 @@ export default function ListsIndex({ lists, flash }: Props) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Lists" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4"></div>
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                {
+                    showToast && (
+                        <div></div>
+                    )
+                }
+            </div>
         </AppLayout>
     )
 }
